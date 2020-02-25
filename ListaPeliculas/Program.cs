b7365a14f696e7b9351b0b2c368634f3dbeded0a
CamaradaNicolas <60996691+CamaradaNicolas@users.noreply.github.com>
2020-02-24 22:04:45 -0800

    ListaPeliculas

---
+++ ListaPeliculas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaPeliculas
+{
+   class Pelicula
+   {
+       private string Titulo ;
+       private int Año;
+       public Pelicula (string titulo , int año)
+       {
+         this.Titulo=titulo;
+         this.Año=año;
+       }
+       public void Imprime ()
+       {
+       Console.WriteLine(Titulo + " " + Año );
+       }
+
+   }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pelicula>Peliculas=new List<Pelicula>();
+             Peliculas.Add(new Pelicula ("Interstellar" ,2014));
+             Peliculas.Add(new Pelicula ("Inception",2010 ));
+             Peliculas.Add(new Pelicula ("Hacksaw Ridge",2016 ));
+             Peliculas.Add(new Pelicula ("The Pianist",2002 ));
+             Peliculas.Add(new Pelicula ("Matrix", 1999 ));
+            
+            foreach(Pelicula a in Peliculas)
+            a.Imprime();
+
+        }
+    }
+}
