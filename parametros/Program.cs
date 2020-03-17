ba1efe6d6b355b1e37479cda2749be3728eaa41a
Gustavo Nicolas Garcia <60996691+CamaradaNicolas@users.noreply.github.com>
2020-03-16 20:15:47 -0700

    Cambio a parametro in
    utilizando una referencia

--- parametros/Program.cs
+++ parametros/Program.cs
@@ -4,19 +4,19 @@ namespace parametros
 {
     class Valor
     {
-      public void Multi2 (in int n)
+      public void Multi2 ( int n)
       {
           n=n*2;
       }
-      public void SumaUno (in int n)
+      public void SumaUno ( int n)
       {
           n=n+1;
       }
-      public void RestaUno (in int n)
+      public void RestaUno ( int n)
       {
           n=n-1;
       }
-      public void Divi2 (in int n)
+      public void Divi2 ( int n)
       {
           n=n/2;
       }
@@ -33,45 +33,45 @@ namespace parametros
     }
     class Restringido
     {
-        static public void Censura( Usuario u)
+        static public void Censura(in Usuario u)
         {
             u.Nombre = "xxxxxxxxxx";
             u.Contraseña = "xxxxxxxxx";
         }
-        static public void Cambia(Usuario u)
+        static public void Cambia(in Usuario u)
         {
             u = new Usuario();
             u.Nombre = "Usuario";
             u.Contraseña ="Contraseña";
         }
-        //static void Main()
-        //{
-            //Usuario Per = new Usuario();
-            //Console.WriteLine(Per.Nombre);
-            //Console.WriteLine(Per.Contraseña);
-            //Censura(Per);
-            //Console.WriteLine(Per.Contraseña);
-            //Cambia(ref Per);
-            //Console.WriteLine(Per.Nombre);
-            //Cambia(ref Per);
-            //Console.WriteLine(Per.Contraseña);
-        //}
-    }
-    
-    class Program
-    {
         static void Main()
         {
-            Valor p = new Valor();
-            int a = 2;int b = 2;int c = 2; int d =2; 
-            p.SumaUno(in a); //Error "No se puede asignar a variable 'in int'por que es una variable ReadOnly."
-            p.RestaUno(in b); //Error "No se puede asignar a variable 'in int'por que es una variable ReadOnly."
-            p.Multi2(in c); //Error "No se puede asignar a variable 'in int'por que es una variable ReadOnly."
-            p.Divi2(in d); //Error "No se puede asignar a variable 'in int'por que es una variable ReadOnly."
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);            
+            Usuario Per = new Usuario();
+            Console.WriteLine(Per.Nombre);
+            Console.WriteLine(Per.Contraseña);
+            Censura(in Per); // Error "No se puede asignar a vairable 'in Usuario' por que es una variable ReadOnly."
+            Console.WriteLine(Per.Contraseña);
+            Cambia(in Per); // Error "No se puede asignar a vairable 'in Usuario' por que es una variable ReadOnly."
+            Console.WriteLine(Per.Nombre);
+            Cambia(in Per); // Error "No se puede asignar a vairable 'in Usuario' por que es una variable ReadOnly."
+            Console.WriteLine(Per.Contraseña);
         }
     }
+    
+    //class Program
+    //{
+        //static void Main()
+        //{
+            //Valor p = new Valor();
+            //int a = 2;int b = 2;int c = 2; int d =2; 
+            //p.SumaUno(in a); 
+            //p.RestaUno(in b);
+            //p.Multi2(in c); 
+            //p.Divi2(in d); 
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+            //Console.WriteLine(d);            
+        //}
+    //}
 }
